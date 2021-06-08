433122fda7f3b4c4791ef535af4cf0eb99137b10
JoasVieira <joasdouradina@gmail.com>
2021-06-07 22:40:12 -0300

    Finishing the Java website project

--- hackathon-site/src/main/java/com/unialfa/hackathonsite/controller/IndexController.java
+++ hackathon-site/src/main/java/com/unialfa/hackathonsite/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.unialfa.hackathonsite.controller;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,10 +30,17 @@ public class IndexController {
 	}
 
 	@RequestMapping("/veiculos/{tipo}")
-	public String novos(@PathVariable String tipo, Model model) {
+	public String tipos(@PathVariable String tipo, Model model) {
 		model.addAttribute("veiculos", veiculoRepository.findByTipo(tipo));
 		model.addAttribute("tipo", tipo);
 
 		return "veiculos/tipo";
+	}
+	
+	@RequestMapping("/veiculo/detalhe")
+	public String detalhes(@PathParam(value = "id") Long id, Model model) {
+		model.addAttribute("veiculo", veiculoRepository.getById(id));
+
+		return "veiculos/detalhe";
 	}
 }
